688591135ea166c2d2098e65880329c1c80a5dfe
majinpeng <2061363265@qq.com>
2020-10-04 18:37:09 +0800

    第一次作业

---
+++ build/precache-manifest.724b317a733ace55d7092bfc7085a417.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b248242c88700ad9272d2d9052cbc489",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "./static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842f1c15a791ffb6d9dc",
+    "url": "./static/js/main.e1eeaa86.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  }
+]);
